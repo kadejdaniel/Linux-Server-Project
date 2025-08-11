781304b89121b9f05b4867049515fbfb77f85013
kadejdaniel <77daniel.kontakt@gmail.com>
2025-08-11 16:44:05 +0000

    dodaje commit test skryptu

---
+++ git_scripts/git1st.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [-z "$1"]; then
+echo "Uzycie skryptu wyglada nastepujaco:./updategit.sh "wiadomosc commita" "
+exit 1
+fi
+git add .
+git commit -m "$1"
+git push origin $(git branch --show-current)
+
